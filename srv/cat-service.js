c16c5f3e98b367640acdb4d618f20263c6bc8597
Lucas Albuquerque Nascimento <lnascimento.ec@gmail.com>
2025-06-30 21:49:29 +0000

    problem: nao enocntra modelo Acompanhamentos

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -7,36 +7,27 @@ const validStatusCriacao = ['Criada', 'Encaminhada', 'Aceita', 'Rejeitada', 'Col
 class MyServices extends cds.ApplicationService {
     init() {
         const { Coletas, Pedidos, Acompanhamentos, Status } = this.entities
-        console.log("Entidades disponíveis:", Object.keys(this.entities));
 
         // Validação de CNPJ e status e status de coleta ao criar
         this.before("POST", Coletas, async (req) => {
             const cnpj = req.data["cnpj_fornecedor"];
-            
+
             try {
-                const cnpjValido = await validarCNPJ(cnpj)
-                // console.log(`cnpjValido: ${cnpjValido}`)
-                if (!cnpjValido) {
-                    return req.error(400, "CNPJ inválido ou não encontrado");
-                };
+                await validarCNPJ(cnpj)
             } catch (error) {
                 return req.error(400, error.message);
             };
 
-            // Usar o stringfy ou o JS usa o toString()
             const pedidos = req.data.pedidos;
-            // console.log(`req: ${JSON.stringify(req)}`)
-            // console.log(`req.data: ${JSON.stringify(req.data)}`)
-            // console.log(`req.data.pedidos: ${JSON.stringify(req.data.pedidos)}`)
-            // console.log(`pedidosId: ${pedidos}`)
+
             if (pedidos) {
                 const pedidosId = pedidos.map(pedido => pedido.ID) // Captura apenas os IDs dos pedidos
-                // console.log(`IDs dos pedidos: ${JSON.stringify(pedidosId)}`)
-                console.log('pedidosID definido')
+                console.log("Entidades disponíveis:", Object.keys(this.entities));
+                console.log(`pedidosID definido ${JSON.stringify(pedidosId)}`)
                 const coletaExistente = await cds.run(
                     SELECT.from(Coletas).alias('c') // Busca uma coleta
-                        .join(Acompanhamentos).as('a').on('a.coleta = c.ID') // Adicoina os acompanhamentos da coleta
-                        .join(Status).as('s').on('a.status = s.status') // Adiciona Status do acompanhamento
+                        .join(Acompanhamentos).alias('a').on('a.coleta = c.ID') // Adicoina os acompanhamentos da coleta
+                        .join(Status).alias('s').on('a.status = s.status') // Adiciona Status do acompanhamento
                         .where({
                             's.status': { in: validStatusCriacao },
                             'c.cnpj_fornecedor': cnpj
--- srv/validation_functions.js
+++ srv/validation_functions.js
@@ -2,13 +2,15 @@ import cds from "@sap/cds";
 
 export async function validarCNPJ(cnpj) {
 
-    if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
-        return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
+    if (!/^\d+$/.test(cnpj)) {
+        throw new Error("CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números");
     };
 
     try {
+        console.log("teste")
         const brasilapi = await cds.connect.to("BrasilAPI_CNPJ");
         const result = await brasilapi.get(`/cnpj/v1/${cnpj}`);
+        console.log(`result: ${result}`)
         return true;
     } catch (error) {
         console.error(error.message);
