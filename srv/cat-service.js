feb6245aa4391312478c3a2cf7d095d6ad14815b
Lucas Albuquerque Nascimento <lnascimento.ec@gmail.com>
2025-06-30 21:05:45 +0000

    refat: organizar import/export do valida_functions

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,43 +1,18 @@
-import cds from "@sap/cds";
-import { results } from "@sap/cds/lib/utils/cds-utils.js";
-// import validarCNPJ from "./validation_functions";
+import cds from '@sap/cds';
+import { validarCNPJ } from './validation_functions.js'; 
 
-async function validarCNPJ(cnpj) {
-    // console.log("entrei na função validar")
-    // try {
-    //     console.log(`cnpj: ${cnpj}`)
-    //     const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);     
-    //     console.log(`response.status: ${response.status}`)   
-        
-    //     // Se nãofor ok, lê como texto e lança erro
-    //     if (response.ok) {
-    //         const erro = await response.text();
-    //         throw new Error(erro);
-    //     };
-
-    //     // Se status 200, lê JSON e retorna true
-    //     const data = await response.json();
-        return true;
-
-    // } catch (error) {
-    //     throw new Error(`Falha na validação: ${error.message}`);
-    // }
-};
 
 const validStatusCriacao = ['Criada', 'Encaminhada', 'Aceita', 'Rejeitada', 'Coletada'];
 
 class MyServices extends cds.ApplicationService {
     init() {
         const { Coletas, Pedidos, Acompanhamentos, Status } = this.entities
+        console.log("Entidades disponíveis:", Object.keys(this.entities));
 
         // Validação de CNPJ e status e status de coleta ao criar
         this.before("POST", Coletas, async (req) => {
             const cnpj = req.data["cnpj_fornecedor"];
-
-            if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
-                return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
-            };
-
+            
             try {
                 const cnpjValido = await validarCNPJ(cnpj)
                 // console.log(`cnpjValido: ${cnpjValido}`)
@@ -59,13 +34,15 @@ class MyServices extends cds.ApplicationService {
                 // console.log(`IDs dos pedidos: ${JSON.stringify(pedidosId)}`)
                 console.log('pedidosID definido')
                 const coletaExistente = await cds.run(
-                    SELECT.one.from(Coletas).alias('c') // Busca uma coleta
-                        .join(Acompanhamentos).alias('a').on('a.coleta = c') // Adicoina os acompanhamentos da coleta
-                        .join(Status).alias('s').on('a.status = s') // Adiciona Status do acompanhamento
-                        .where('s.status in', validStatusCriacao) 
-                        .and('c_cnpj_fornecedor =', cnpj) // Filtra pelo mesmo fornecedor da coleta atual
-                        .and(`EXISTS (SELECT 1 FROM vendas.pedidos p WHERE p.coleta_ID = c.ID AND p.ID in`, pedidosId, ')') // Só retorna se tiver pedidos associados a coleta
+                    SELECT.from(Coletas).alias('c') // Busca uma coleta
+                        .join(Acompanhamentos).as('a').on('a.coleta = c.ID') // Adicoina os acompanhamentos da coleta
+                        .join(Status).as('s').on('a.status = s.status') // Adiciona Status do acompanhamento
+                        .where({
+                            's.status': { in: validStatusCriacao },
+                            'c.cnpj_fornecedor': cnpj
+                        }) // Filtra pelo mesmo fornecedor da coleta atual                        
                 );
+
                 console.log(`coletaExistente: ${coletaExistente}`)
                 if (coletaExistente) {
                     return req.error(400, 'já existe uma coleta vinculada a este pedido')
--- srv/validation_functions.js
+++ srv/validation_functions.js
@@ -1,18 +1,30 @@
-// export async function validarCNPJ(cnpj) {
-//     console.log("entrei na função validar")
-//     try {
-//         const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);
-//         const data = await response.json();
-//         console.log(data)
+export async function validarCNPJ(cnpj) {
+    console.log("entrei na função validar")
+    try {
+        console.log(`cnpj: ${cnpj}`)
+        if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
+            return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
+        };
+        
+        const api = {
+            url: `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`,
+            format: 'json',
+            kind: 'rest'
+        }
+        const response = await cds.import.from.asyncapi(api);     
+        console.log(`response.status: ${response.status}`)   
 
+        // Se nãofor ok, lê como texto e lança erro
+        if (response.ok) {
+            const erro = await response.text();
+            throw new Error(erro);
+        };
 
-//         if (!response.ok) {
-//             throw new Error(data.message || `Erro na API: ${response.status}`);
-            
-//         }
-//         return true;
+        // Se status 200, lê JSON e retorna true
+        const data = await response.json();
+    return true;
 
-//     } catch (error) {
-//         throw new Error(`Falha na validação: ${error.message}`);
-//     }
-// };
+    } catch (error) {
+        throw new Error(`Falha na validação: ${error.message}`);
+    }
+};
