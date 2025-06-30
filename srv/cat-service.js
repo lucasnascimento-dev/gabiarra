202382f856bbd0283b06ff3b5b177c7f0780176f
Lucas Albuquerque Nascimento <lnascimento.ec@gmail.com>
2025-06-30 14:13:15 +0000

    feat: adiciona logica do status de acompanhamento

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,31 +1,62 @@
 import cds from "@sap/cds";
-// import { MyService } from "../db";
-// import { validarCNPJ } from "./validation_functions";
+import { results } from "@sap/cds/lib/utils/cds-utils";
+// import validarCNPJ from "./validation_functions";
+
+async function validarCNPJ(cnpj) {
+    console.log("entrei na função validar")
+    try {
+        const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);
+        const data = await response.json();
+        console.log(data);
+
+
+        if (!response.ok) {
+            throw new Error(data.message || `Erro na API: ${response.status}`);
+        };
+        return true;
+
+    } catch (error) {
+        throw new Error(`Falha na validação: ${error.message}`);
+    }
+};
+
+const validStatusCriacao = ['Criada', 'Encaminhada', 'Aceita', 'Rejeitada', 'Coletada'];
 
 class MyServices extends cds.ApplicationService {
     init() {
         const { Coletas, Pedidoa, Acompanhamentos } = this.entities
 
-        this.before("READ", Coletas, async (req) => {
-            console.log('TESTE1');
-            
-            //se existir . ou - gerar info pedindo apenas números
+        // Validação de CNPJ e status e status de coleta ao criar
+        this.before("POST", Coletas, async (req) => {
+            const cnpj = req.data["cnpj_fornecedor"];
 
-            // if (!cnpj_fornecedor) {
-            //     return req.error(400, "CNPJ do fornecedor é obrigatório.")
-            // }
+            if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
+                return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
+            };
 
-            // const cnpjValido = await validarCNPJ(cnpj_fornecedor)
+            try {
+                const cnpjValido = await validarCNPJ(cnpj)
+                if (!cnpjValido) {
+                    return req.error(400, "CNPJ inválido ou não encontrado");
+                };
+            } catch (error) {
+                return req.error(400, error.message);
+            };
 
-            // if (!cnpjValido) {
-            //     return req.error(400, "CNPJ Inválido.")
-            // }
-        })
-        this.on("READ", Coletas, async (data, req) => {
-            console.log(`TESTE2`);
-        })       
-        this.after("READ", Coletas, async (data, req) => {
-            console.log(`TESTE3`);
+            const pedidos = req.data.pedidos;
+            if (pedidos) {
+                const pedidosId = pedidos.map((pedidos) => pedidos.ID) // Captura apenas os IDs dos pedidos
+
+                const coletaExistente = await cds.run(
+                    SELECT.one.from(Coletas).alias('c') // Busca uma coleta
+                        .join(Acompanhamentos).alias('a').on('a.coleta_ID = c.ID') // Adicoina os acompanhamentos da coleta
+                        .join(Status).alias('s').on('a.status_status = s.status') // Adiciona Status do acompanhamento
+                        .where('s.status in', validStatusCriacao)
+                        .and(`EXISTS (SELECT 1 FROM vendas.pedidos p WHERE p.coleta_ID = c.ID AND p.ID in`, pedidosId, ')') // Só retorna se tiver pedidos associados a coleta
+                )
+
+            }
+
         })
 
         return super.init();
--- srv/validation_functions.js
+++ srv/validation_functions.js
@@ -1,15 +1,18 @@
+// export async function validarCNPJ(cnpj) {
+//     console.log("entrei na função validar")
+//     try {
+//         const response = await fetch(`https://brasilapi.com.br/api/cnpj/v1/${cnpj}`);
+//         const data = await response.json();
+//         console.log(data)
 
-async function validarCNPJ(cnpj_fornecedor) {
-    try {
-        const response = await axios.get(`https://brasilapi.com.br/api/cnpj/v1/${cnpj_fornecedor}`);
-        if (response.cnpj) {
-            return true;
-        }
-    } catch (error) {
-        if (!error.data) {
-            throw new Error("Erro ao consultar API");
-        } else {
-            return error.message
-        };
-    }
-}
+
+//         if (!response.ok) {
+//             throw new Error(data.message || `Erro na API: ${response.status}`);
+            
+//         }
+//         return true;
+
+//     } catch (error) {
+//         throw new Error(`Falha na validação: ${error.message}`);
+//     }
+// };
