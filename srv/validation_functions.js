bd509bcc93959dde77ce9ea10f46bfc1ef36a760
Lucas Albuquerque Nascimento <lnascimento.ec@gmail.com>
2025-06-30 21:26:48 +0000

    refat: ajusta conexao com api

--- srv/validation_functions.js
+++ srv/validation_functions.js
@@ -1,30 +1,18 @@
+import cds from "@sap/cds";
+
 export async function validarCNPJ(cnpj) {
-    console.log("entrei na função validar")
+
+    if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
+        return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
+    };
+
     try {
-        console.log(`cnpj: ${cnpj}`)
-        if (!/^\d+$/.test(cnpj) && cnpj.length == 14) {
-            return req.error(400, "CNPJ deve conter exatamente 14 dígitos e ser composto apenas por números")
-        };
-        
-        const api = {
-            url: `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`,
-            format: 'json',
-            kind: 'rest'
-        }
-        const response = await cds.import.from.asyncapi(api);     
-        console.log(`response.status: ${response.status}`)   
-
-        // Se nãofor ok, lê como texto e lança erro
-        if (response.ok) {
-            const erro = await response.text();
-            throw new Error(erro);
-        };
-
-        // Se status 200, lê JSON e retorna true
-        const data = await response.json();
-    return true;
-
+        const brasilapi = await cds.connect.to("BrasilAPI_CNPJ");
+        const result = await brasilapi.get(`/cnpj/v1/${cnpj}`);
+        return true;
     } catch (error) {
-        throw new Error(`Falha na validação: ${error.message}`);
+        console.error(error.message);
+        throw new Error("CNPJ inválido ou não encontrado");
+
     }
 };
